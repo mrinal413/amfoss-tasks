f9a8d468f910f72dcf1b5ed89478347b6c57d76c
mrinal413 <73014353+mrinal413@users.noreply.github.com>
2020-11-30 16:30:01 +0530

    Create plurality.c

---
+++ Task 10/pset3/plurality.c
@@ -0,0 +1,88 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX 9
+
+typedef struct
+{
+    string name;
+    int votes;
+}
+candidate;
+
+candidate candidates[MAX];
+
+int candidate_count;
+
+bool vote(string name);
+void print_winner(void);
+
+int main(int c, string v[])
+{
+    if (c < 2)
+    {
+        printf("Usage: plurality [candidate ...]\n");
+        return 1;
+    }
+
+    candidate_count = c - 1;
+    if (candidate_count > MAX)
+    {
+        printf("Maximum number of candidates is %i\n", MAX);
+        return 2;
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].name = v[i + 1];
+        candidates[i].votes = 0;
+    }
+
+    int voter_count = get_int("Number of voters: ");
+
+    for (int i = 0; i < voter_count; i++)
+    {
+        string name = get_string("Vote: ");
+
+        if (!vote(name))
+        {
+            printf("Invalid vote.\n");
+        }
+    }
+
+    print_winner();
+}
+
+bool vote(string name)
+{
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
+    return false;
+}
+
+void print_winner(void)
+{
+    int maxvotes = 0;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > maxvotes)
+        {
+            maxvotes = candidates[i].votes;
+        }
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes == maxvotes)
+        {
+            printf("%s\n", candidates[i].name);
+        }
+    }
+
+}
